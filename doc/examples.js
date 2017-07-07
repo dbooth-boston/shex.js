d76ae08ccf6d52a3b563a9917072b59ac311f467
andrawaag <andra@micelio.be>
2017-07-07 15:01:56 +0200

    started wikicite stubs

--- doc/examples.js
+++ doc/examples.js
@@ -1,86 +1,23 @@
 Demos = (() => {
-var clinicalObs = {};
+var wikicite_paper = {};
+var wikicite_proceedings = {};
+var wikicite_book = {};
 
-clinicalObs.schema = `PREFIX : <http://hl7.org/fhir/>
-PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+wikicite_paper.schema = ``;
+wikicite_paper.success = ``;
+wikicite_paper.fail = ``;
 
-start = @<ObservationShape>
+wikicite_proceedings.schema = ``;
+wikicite_proceedings.success = ``;
+wikicite_proceedings.fail = ``;
 
-<ObservationShape> {               # An Observation has:
-  :status ["preliminary" "final"]; #   status in this value set
-  :subject @<PatientShape>         #   a subject matching <PatientShape>.
-}
-
-<PatientShape> {                   # A Patient has:
- :name xsd:string*;                #   one or more names
- :birthdate xsd:date?              #   and an optional birthdate.
-}
-`;
-clinicalObs.with_birthdate = `PREFIX : <http://hl7.org/fhir/>
-PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-
-<Obs1>
-  :status    "final" ;
-  :subject   <Patient2> .
-
-<Patient2>
-  :name "Bob" ;
-  :birthdate "1999-12-31"^^xsd:date .`;
-clinicalObs.no_subject_name = `PREFIX : <http://hl7.org/fhir/>
-PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-
-<Obs1>
-  :status    "final" ;
-  :subject   <Patient2> .
-
-<Patient2>
-  :birthdate "1999-12-31"^^xsd:date .`;
-clinicalObs.without_birthdate = `PREFIX : <http://hl7.org/fhir/>
-PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-
-<Obs1>
-  :status    "preliminary" ;
-  :subject   <Patient2> .
-
-<Patient2>
-  :name "Bob" .`;
-clinicalObs.bad_status = `PREFIX : <http://hl7.org/fhir/>
-PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-
-<Obs1>
-  :status    "finally" ;
-  :subject   <Patient2> .
-
-<Patient2>
-  :name "Bob" ;
-  :birthdate "1999-12-31"^^xsd:date .
-
-`;
-clinicalObs.no_subject = `PREFIX : <http://hl7.org/fhir/>
-PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-
-<Obs1>
-  :status    "final" .
-
-<Patient2>
-  :name "Bob" ;
-  :birthdate "1999-12-31"^^xsd:date .
-
-`;
-clinicalObs.birthdate_datatype = `PREFIX : <http://hl7.org/fhir/>
-PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-
-<Obs1>
-  :status    "final" ;
-  :subject   <Patient2> .
-
-<Patient2>
-  :name "Bob" ;
-  :birthdate "1999-12-31T01:23:45"^^xsd:dateTime .`;
+wikicite_book.schema = ``;
+wikicite_book.success = ``;
+wikicite_book.fail = ``;
 
   return {
-    "clinical observation": {
-      schema: clinicalObs.schema,
+    "Biomedical scientific article": {
+      schema: wikicite_paper.schema,
       passes: {
         "with birthdate": {
           data: clinicalObs.with_birthdate,
@@ -103,6 +40,56 @@ PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
           data: clinicalObs.birthdate_datatype,
           queryMap: "<http://a.example/Obs1>@- start -"}
       }
+    }
+    "Proceedings": {
+          schema: wikicite_proceedings.schema,
+          passes: {
+            "with birthdate": {
+              data: clinicalObs.with_birthdate,
+              queryMap: "{FOCUS :status _}@- start -,\n<Patient2>@<http://a.example/ObservationShape>"},
+            "without birthdate": {
+              data: clinicalObs.without_birthdate,
+              queryMap: "<http://a.example/Obs1>@- start -"},
+            "no subject name": {
+              data: clinicalObs.no_subject_name,
+              queryMap: "<http://a.example/Obs1>@- start -"}
+          },
+          fails: {
+            "bad status": {
+              data: clinicalObs.bad_status,
+              queryMap: "<http://a.example/Obs1>@- start -"},
+            "no subject": {
+              data: clinicalObs.no_subject,
+              queryMap: "<http://a.example/Obs1>@- start -"},
+            "wrong birthdate datatype": {
+              data: clinicalObs.birthdate_datatype,
+              queryMap: "<http://a.example/Obs1>@- start -"}
+          }
+    }
+    "English book": {
+          schema: wikicite_book.schema,
+          passes: {
+            "with birthdate": {
+              data: clinicalObs.with_birthdate,
+              queryMap: "{FOCUS :status _}@- start -,\n<Patient2>@<http://a.example/ObservationShape>"},
+            "without birthdate": {
+              data: clinicalObs.without_birthdate,
+              queryMap: "<http://a.example/Obs1>@- start -"},
+            "no subject name": {
+              data: clinicalObs.no_subject_name,
+              queryMap: "<http://a.example/Obs1>@- start -"}
+          },
+          fails: {
+            "bad status": {
+              data: clinicalObs.bad_status,
+              queryMap: "<http://a.example/Obs1>@- start -"},
+            "no subject": {
+              data: clinicalObs.no_subject,
+              queryMap: "<http://a.example/Obs1>@- start -"},
+            "wrong birthdate datatype": {
+              data: clinicalObs.birthdate_datatype,
+              queryMap: "<http://a.example/Obs1>@- start -"}
+          }
     }
   };
 })();
